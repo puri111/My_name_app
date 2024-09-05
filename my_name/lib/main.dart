f8c3a17d9b18871a56002ea6fd70b43ce2c75127
purity <purityamaechi721|@gmail.com>
2024-09-05 14:03:31 +0100

    first commit

---
+++ my_name/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp ( 
+ 
+ MaterialApp(
+      home:  Scaffold(
+        appBar: AppBar( 
+         title: const Text('My Name App'),
+          ),
+             body: const Center(
+            child: Text('PURIFIED!'),
+           ),
+        ),
+      )
+    );
+     }
+     
+
+ 
+ 
